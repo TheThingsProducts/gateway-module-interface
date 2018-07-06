42df538650189852d1b8e415d4cec3c6fca74ad0
Marten Lootsma <marten@twtg.io>
2018-07-06 09:35:45 +0200

    Initial implementation

---
+++ lib/gateway-module-interface.h
@@ -0,0 +1,59 @@
+// Copyright © 2016-2018 The Things Products
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+
+#ifndef LIB_GATEWAY_MODULE_INTERFACE_H_
+#define LIB_GATEWAY_MODULE_INTERFACE_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+
+typedef enum
+{
+    GATEWAY_MODULE_CMD_NONE 			= 0,
+    GATEWAY_MODULE_CMD_FACTORY 			= 0x6E, // Host -> Module Restores the factory default settings.
+    GATEWAY_MODULE_CMD_SAVE 			= 0x21, // Host -> Module Saves the current settings to EEPROM.
+    GATEWAY_MODULE_CMD_SETUART 			= 0x42, // Host -> Module Sets the UART baud rate to the Host.
+    GATEWAY_MODULE_CMD_GETUART 			= 0x62, // Host -> Module Returns the Host UART baud rate setting.
+    GATEWAY_MODULE_CMD_START 			= 0x30, // Host -> Module Startup RF Module.
+    GATEWAY_MODULE_CMD_STOP 			= 0x31, // Host -> Module Stop RF Module.
+    GATEWAY_MODULE_CMD_SEND 			= 0x32, // Host -> Module Host Send LoRa Packet.
+    GATEWAY_MODULE_CMD_RECEIVE 			= 0x33, // Module -> Host Host Receive LoRa Packet.
+    GATEWAY_MODULE_CMD_RFCONFIG 		= 0x34, // Host -> Module Configure RF(0-1) chain center frequency.
+    GATEWAY_MODULE_CMD_IFCONFIG 		= 0x35, // Host -> Module Configure IF chain(0-7) center frequency.
+    GATEWAY_MODULE_CMD_IF8CONFIG 		= 0x36, // Host -> Module Configure IF8 chain Bandwidth and Data Rate.
+    GATEWAY_MODULE_CMD_IF9CONFIG 		= 0x37, // Host -> Module Configure IF9 chain Bandwidth and Data Rate.
+    GATEWAY_MODULE_CMD_TXABORT 			= 0x38, // Host -> Module Aborts any current or scheduled transmission.
+    GATEWAY_MODULE_CMD_TXSTATUS 		= 0x39, // Host -> Module Returns radio transmitter status.
+    GATEWAY_MODULE_CMD_VERSION 			= 0x3A, // Host -> Module Returns Module version information.
+    GATEWAY_MODULE_CMD_RFCHAIN 			= 0x3B, // Host -> Module Returns RF(0-1) chain settings.
+    GATEWAY_MODULE_CMD_IFCHAIN 			= 0x3C, // Host -> Module Returns IF chain(0-7) settings.
+    GATEWAY_MODULE_CMD_IF8CHAIN 		= 0x3D, // Host -> Module Returns IF8 chain settings.
+    GATEWAY_MODULE_CMD_IF9CHAIN 		= 0x3E, // Host -> Module Returns IF9 chain settings.
+    GATEWAY_MODULE_CMD_SETLEDS 			= 0x3F, // Host -> Module Sets the output LED’s state.
+    GATEWAY_MODULE_CMD_SETSYNC 			= 0x40, // Host -> Module Sets the LoRa IF channels Sync Word.
+    GATEWAY_MODULE_CMD_GETSYNC 			= 0x41, // Host -> Module Returns the LoRa IF channels Sync Word.
+    GATEWAY_MODULE_CMD_RXSTATUS 		= 0x43, // Host -> Module Returns the LoRa Receive status.
+    GATEWAY_MODULE_CMD_BOOTLOADER_MODE	= 0x50, // Host -> Module Invalidates the checksum of the application. Need to followed by a RESET command to switch to the Bootloader.
+    GATEWAY_MODULE_CMD_RESET 			= 0x51, // Host -> Module Resets the module
+    GATEWAY_MODULE_CMD_SENDCW 			= 0x25, // Host -> Module Enable CW Mode (Continuous Transmit)
+    GATEWAY_MODULE_CMD_INVALID 			= 0xFF, // Module -> Host Previous command received from Host does not exist
+    GATEWAY_MODULE_CMD_MFGDATA 			= 0x07  // Host -> Module Program Manufact
+} GATEWAY_MODULE_CMDS_t;
+
+typedef void (*gateway_module_interface_write_lock_t)(bool lock);
+typedef bool (*gateway_module_interface_write_t)(uint8_t *data, size_t size);
+typedef bool (*gateway_module_signal_wait_t)(int timeout);
+typedef void (*gateway_module_signal_set_t)(void);
+typedef void (*gateway_module_receive_callback_t)(uint8_t *data, size_t size);
+typedef void (*gateway_module_log_t) (const char * format, ...);
+
+
+void GatewayModuleInterface_init(gateway_module_interface_write_lock_t write_lock, gateway_module_interface_write_t write, gateway_module_signal_wait_t signal_wait, gateway_module_signal_set_t signal_set, gateway_module_receive_callback_t receive_callback, gateway_module_log_t log);
+bool GatewayModuleInterface_sendCommandWaitAnswer(GATEWAY_MODULE_CMDS_t cmd, uint8_t *cmd_payload, size_t cmd_payload_size, uint8_t *ans_payload, size_t ans_payload_max_size);
+bool GatewayModuleInterface_sendCommandWaitAck(GATEWAY_MODULE_CMDS_t cmd, uint8_t *cmd_payload, size_t cmd_payload_size);
+void GatewayModuleInterface_sendAck(GATEWAY_MODULE_CMDS_t cmd, bool ack);
+void GatewayModuleInterface_dispatch(uint8_t d);
+
+#endif /* LIB_GATEWAY_MODULE_INTERFACE_H_ */
---
+++ linux/main.cpp
@@ -0,0 +1,189 @@
+// Copyright © 2016-2018 The Things Products
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file.
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <iostream>
+#include <thread>
+#include <stdarg.h>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+
+extern "C"
+{
+#include "gateway-module-interface.h"
+}
+
+using namespace std;
+
+static bool init_uart(const char *uart);
+static void lock_uart(bool lock);
+static bool write_uart(uint8_t *data, size_t size);
+static bool signal_wait(int timeout);
+static void signal_set(void);
+static void receive_callback(uint8_t *data, size_t size);
+static void dispatch_thread(void);
+
+void LOG(const char *__restrict __format, ...);
+
+const char* UART_NAME = "/dev/ttyUSB0";
+static int _uart_fs = -1;
+static mutex lock_m;
+static mutex signal_m;
+static condition_variable signal_cv;
+
+int main()
+{
+    if (!init_uart(UART_NAME))
+    {
+        LOG("Failed to open '%s'. Make sure is does exist and is not opened by anyone else.", UART_NAME);
+        return -1;
+    }
+
+    GatewayModuleInterface_init(&lock_uart, &write_uart, &signal_wait,
+            &signal_set, &receive_callback, &LOG);
+
+    thread t(dispatch_thread);
+
+    struct
+    {
+        uint8_t band;
+        uint8_t hwrev;
+        uint8_t serial_number[12];
+        uint8_t minor;
+        uint8_t major;
+    } version;
+
+    // send some invalid command
+    LOG("> send some invalid command");
+    GatewayModuleInterface_sendCommandWaitAnswer((GATEWAY_MODULE_CMDS_t) 8,
+            NULL, 0, NULL, 0);
+
+    // send version request
+    LOG("> send version request");
+    if (GatewayModuleInterface_sendCommandWaitAnswer(GATEWAY_MODULE_CMD_VERSION,
+            NULL, 0, (uint8_t*) &version, sizeof(version)))
+    {
+        LOG("Version, hwrev: %d, major: %d, minor: %d, band: %d", version.hwrev,
+                version.major, version.minor, version.band);
+        LOG(
+                "Serial: %02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X",
+                version.serial_number[0], version.serial_number[1],
+                version.serial_number[2], version.serial_number[3],
+                version.serial_number[4], version.serial_number[5],
+                version.serial_number[6], version.serial_number[7],
+                version.serial_number[8], version.serial_number[9],
+                version.serial_number[10], version.serial_number[11]);
+    }
+    else
+    {
+        LOG("Failed to ready version");
+    }
+
+    // send receive nack, to trigger current message in rx queue to be replied (if any)
+    LOG("> send receive nack, to trigger current message in rx queue to be replied (if any)");
+    GatewayModuleInterface_sendAck(GATEWAY_MODULE_CMD_RECEIVE, false);
+
+    t.join();
+
+    return 0;
+}
+
+static bool init_uart(const char *uart)
+{
+    _uart_fs = open(uart, O_RDWR | O_NOCTTY); //Open in non blocking read/write mode
+    if (_uart_fs == -1)
+    {
+        return false;
+    }
+
+    struct termios options;
+    tcgetattr(_uart_fs, &options);
+    options.c_cflag = B115200 | CS8 | CLOCAL | CREAD;
+    options.c_iflag = IGNPAR;
+    options.c_oflag = 0;
+    options.c_lflag = 0;
+    tcflush(_uart_fs, TCIFLUSH);
+    tcsetattr(_uart_fs, TCSANOW, &options);
+
+    return true;
+}
+
+static void lock_uart(bool lock)
+{
+    if (lock)
+    {
+        LOG("Lock");
+        lock_m.lock();
+    }
+    else
+    {
+        LOG("Unlock");
+        lock_m.unlock();
+    }
+}
+
+static bool write_uart(uint8_t *data, size_t size)
+{
+    if (size > 0)
+    {
+        size_t w = write(_uart_fs, data, size);
+        if (w != size)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+static bool signal_wait(int timeout)
+{
+    LOG("Wait signal");
+    unique_lock<mutex> lck(signal_m);
+    return signal_cv.wait_for(lck, chrono::milliseconds(timeout))
+            != cv_status::timeout;
+}
+
+static void signal_set(void)
+{
+    LOG("Set signal");
+    unique_lock<mutex> lck(signal_m);
+    signal_cv.notify_one();
+}
+
+static void receive_callback(uint8_t *data, size_t size)
+{
+    LOG("TODO: Handle received: %i", size);
+}
+
+static void dispatch_thread(void)
+{
+    char c;
+    while (true)
+    {
+        int r = read(_uart_fs, &c, 1);
+        if (r != 1)
+        {
+            LOG("Read returned code: %i", r);
+            break;
+        }
+        GatewayModuleInterface_dispatch(c);
+    }
+
+    cout << "Thread exit." << endl;
+}
+
+void LOG(const char *__restrict __format, ...)
+{
+    va_list args;
+    va_start(args, __format);
+    vprintf(__format, args);
+    printf("\r\n");
+    va_end(args);
+}
+
